9cc82dcef64d3608c5786bb52a2af833a60cbd35
Fred Medlin <fred@twotoasters.com>
2013-05-09 14:16:23 -0400

    Initial commit

---
+++ src/main/scala/com/twotoasters/khanauth/MainActivity.scala
@@ -0,0 +1,20 @@
+package com.twotoasters.khanauth;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+class MainActivity extends Activity {
+
+    override def onCreate(savedInstanceState: Bundle) = {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }    
+
+    override def onCreateOptionsMenu(menu: Menu) = {
+        getMenuInflater().inflate(R.menu.main, menu)
+        true
+    }
+
+}
+
