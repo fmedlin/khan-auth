0cb44f32375f0a1e62e592563788dc381adb4d74
Fred Medlin <fred@twotoasters.com>
2013-05-10 14:22:19 -0400

    Startup authentication for existing accounts

--- src/main/scala/com/twotoasters/khanauth/StartupActivity.scala
+++ src/main/scala/com/twotoasters/khanauth/StartupActivity.scala
@@ -7,10 +7,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 
-class StartupActivity extends Activity {
+class StartupActivity(am : AccountManager) extends Activity {
 
-	val resultCode = 100
-	val account : Account = null
+	var accountManager = am
+
+	val NewAccountCode = 100
+	val ExistingAccountCode = 101
+	val UsernameParam = "username"
+
+	def this() {
+		this(null)
+	}
 
     override def onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
@@ -19,13 +26,25 @@ class StartupActivity extends Activity {
     }
 
     def login {
-    	val am = AccountManager.get(this)
-    	val accounts = am.getAccountsByType("com.twotoasters.khanauth")
+        if (accountManager == null) {
+        	accountManager = AccountManager.get(this)
+        }
+
+    	val accounts = accountManager.getAccountsByType("com.twotoasters.khanauth")
     	if (accounts.isEmpty) {
     		val intent = new Intent(this, classOf[AuthenticatorActivity])
 			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
-			startActivityForResult(intent, resultCode)
+			startActivityForResult(intent, NewAccountCode)
+    	}
+    	else {
+    		val password = accountManager.getPassword(accounts.head)
+    		if (password == null) {
+	    		val intent = new Intent(this, classOf[AuthenticatorActivity])
+	    		intent.putExtra(UsernameParam, accounts.head.name)
+				startActivityForResult(intent, ExistingAccountCode)
+    		}
     	}
     }
+
 }
 
--- src/test/scala/com/twotoasters/khanauth/StartupActvityTest.scala
+++ src/test/scala/com/twotoasters/khanauth/StartupActvityTest.scala
@@ -1,25 +1,56 @@
 package com.twotoasters.khanauth;
 
+import android.accounts.Account
+import android.accounts.AccountManager
+import android.content.Intent
+
+import org.hamcrest.CoreMatchers._
+
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertThat
+import org.junit.Assert.fail
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.robolectric.RobolectricTestRunner
+import org.mockito.Mockito._
+
+import org.robolectric._
+import org.robolectric.Robolectric._
 import org.robolectric.matchers.StartedMatcher
 
 @RunWith(classOf[RobolectricTestRunner])
 class StartupActivityTest {
 
+    val accountManager = mock(classOf[AccountManager])
+	val account = new Account("fred", "com.twotoasters.khanauth")
+
     var activity: StartupActivity = null
 
-    @Before def setup {
+    @Test def itShouldAuthenticateNewAccount {
         activity = new StartupActivity
         activity.onCreate(null)
-    }
 
-    @Test def itShouldAuthenticate {
     	assertThat(activity, new StartedMatcher(classOf[AuthenticatorActivity]))
     }
 
+    @Test def itShouldAuthenticateExistingAccount {
+    	when(accountManager.getAccountsByType("com.twotoasters.khanauth"))
+    		.thenReturn(Array[Account](account))
+
+        activity = new StartupActivity(accountManager)
+        activity.onCreate(null)
+
+        val expected = new Intent()
+        	.setClassName("com.twotoasters.khanauth", classOf[AuthenticatorActivity].getName())
+        	.putExtra("username", "fred")
+
+    	assertThat(shadowOf(activity).getNextStartedActivity(),
+    		equalTo(expected))
+    }
+
+    @Test def itShouldAllowAuthenticatedUsers {
+    	fail("Implement me")
+    }
 }
